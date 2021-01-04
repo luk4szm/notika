bf7bbce76deadff2d2c16681848ff9c5e30a95d1
luk4szm <lukasz@mikowski.pl>
2021-01-04 23:17:55 +0100

    RegistrationFrom added

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
 {
@@ -95,6 +97,11 @@ class User implements UserInterface
 	 * @ORM\OneToMany(targetEntity=Game::class, mappedBy="updated_by")
 	 */
 	private $gamesUpdated;
+
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isVerified = false;
 
 	public function __construct()
 	{
@@ -370,6 +377,18 @@ class User implements UserInterface
 				$gamesUpdated->setUpdatedBy(null);
 			}
 		}
+
+		return $this;
+	}
+
+	public function isVerified(): bool
+	{
+		return $this->isVerified;
+	}
+
+	public function setIsVerified(bool $isVerified): self
+	{
+		$this->isVerified = $isVerified;
 
 		return $this;
 	}

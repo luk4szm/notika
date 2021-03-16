c0998fe1460fea74cf998295dd1e8547f156745d
luk4szm <lukasz@mikowski.pl>
2021-03-16 21:01:24 +0100

    add labels in form

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -6,6 +6,7 @@ use App\Repository\TeamRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=TeamRepository::class)
@@ -30,7 +31,13 @@ class Team
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=3)
+     * @ORM\Column(type="string", length=255)
+     * @Gedmo\Slug(fields={"name"})
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=3, unique=true)
      */
     private $short_name;
 
@@ -85,6 +92,18 @@ class Team
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }

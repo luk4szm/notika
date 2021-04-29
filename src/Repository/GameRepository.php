226426b8e2d2ca05b2e70c5d6d0a805ac6c29a0d
luk4szm <lukasz@mikowski.pl>
2021-04-30 00:23:34 +0200

    findRoundGames method

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Game;
+use App\Entity\Season;
+use App\Entity\Team;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -21,79 +23,65 @@ class GameRepository extends ServiceEntityRepository
 
     /**
      * Find all games for specified team
-     *
      * @param $id
      * @return mixed
      */
-    public function findTeamAllGames($id)
+    public function findTeamGames(Team $team)
     {
         return $this->createQueryBuilder('g')
-            ->andWhere('g.home = :val')
-            ->orWhere('g.guest = :val')
-            ->setParameter('val', $id)
-            ->orderBy('g.date', 'DESC')
-            ->getQuery()
-            ->getResult();
+                    ->andWhere('g.home = :val')
+                    ->orWhere('g.guest = :val')
+                    ->setParameter('val', $team)
+                    ->orderBy('g.date', 'DESC')
+                    ->getQuery()
+                    ->getResult();
     }
 
     /**
      * Find all games for specified season
-     *
      * @param $id
      * @return mixed
      */
-    public function findSeasonAllGames($id)
+    public function findSeasonGames(Season $season)
     {
         return $this->createQueryBuilder('g')
-            ->andWhere('g.season = :val')
-            ->setParameter('val', $id)
-            ->orderBy('g.date', 'ASC')
-            ->getQuery()
-            ->getResult();
+                    ->andWhere('g.season = :val')
+                    ->setParameter('val', $season)
+                    ->orderBy('g.date', 'ASC')
+                    ->getQuery()
+                    ->getResult();
+    }
+
+    /**
+     * Find all games for specified season round
+     * @param Season $season
+     * @param int $round
+     * @return mixed
+     */
+    public function findRoundGames(Season $season, int $round)
+    {
+        return $this->createQueryBuilder('g')
+                    ->andWhere('g.season = :season')
+                    ->setParameter('season', $season)
+                    ->andWhere('g.round = :round')
+                    ->setParameter('round', $round)
+                    ->orderBy('g.date', 'ASC')
+                    ->getQuery()
+                    ->getResult();
     }
 
     /**
      * Find next played game
-     *
      * @return mixed
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function findNextGame()
     {
         return $this->createQueryBuilder('g')
-            ->andWhere('g.date > :val')
-            ->setParameter('val', (new \DateTime()))
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getOneOrNullResult();
+                    ->andWhere('g.date > :val')
+                    ->setParameter('val', (new \DateTime()))
+                    ->setMaxResults(1)
+                    ->getQuery()
+                    ->getOneOrNullResult();
     }
-
-    // /**
-    //  * @return Game[] Returns an array of Game objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Game
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }

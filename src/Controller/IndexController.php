eafe6a1137cf570f7f9496161d0b369ef382181f
luk4szm <lukasz@mikowski.pl>
2021-04-29 23:32:14 +0200

    missing bet query

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Bet;
 use App\Entity\Game;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +15,7 @@ class IndexController extends AbstractController
 
     public function __construct(EntityManagerInterface $em)
     {
-        $this->em = $em;
+        $this->em       = $em;
     }
 
     /**
@@ -22,10 +23,16 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
-        $nextGame = $this->em->getRepository(Game::class)->findNextGame();
+        $nextGame    = $this->em->getRepository(Game::class)->findNextGame();
+        $missingBets = null;
+
+        if ($this->getUser()) {
+            $missingBets = $this->em->getRepository(Bet::class)->countUserMissingBets();
+        }
 
         return $this->render('index/index.html.twig', [
-            'nextGame' => $nextGame,
+            'nextGame'    => $nextGame,
+            'missingBets' => $missingBets,
         ]);
     }
 }
--- src/Repository/BetRepository.php
+++ src/Repository/BetRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Bet;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @method Bet|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,37 +16,85 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class BetRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private $security;
+
+    public function __construct(ManagerRegistry $registry, Security $security)
     {
         parent::__construct($registry, Bet::class);
+        $this->security = $security;
     }
 
-    // /**
-    //  * @return Bet[] Returns an array of Bet objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * Return array of user Bets objects
+     *
+     * @param User|null $user
+     * @return mixed
+     */
+    public function findAllUserBets(User $user = null)
     {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $user = $user ? $user : $this->security->getUser();
 
-    /*
-    public function findOneBySomeField($value): ?Bet
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $this->createQueryBuilder('bet')
+                    ->leftJoin('bet.game', 'game')
+                    ->where('bet.user = :user')
+                    ->setParameter('user', $user)
+                    ->getQuery()
+                    ->getResult();
     }
-    */
+
+    public function findUserMissingBets(User $user = null)
+    {
+        $user = $user ? $user : $this->security->getUser();
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "SELECT t1.* FROM (
+                    SELECT m.*
+                    FROM game m
+                    WHERE m.date > :date
+                ) AS t1
+                LEFT JOIN (
+                    SELECT m.*
+                    FROM game m
+                    LEFT JOIN bet b ON b.game_id = m.ID
+                    WHERE b.user_id = :user
+                ) AS t2
+                ON t1.ID = t2.ID
+                WHERE t2.ID IS NULL";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('date', (new \DateTime("2021-02-06 20:34:00"))->format('Y-m-d H:i:s'));
+        $stmt->bindValue('user', $user->getId());
+        $stmt->execute();
+
+        return $stmt->fetchAllAssociative();
+    }
+
+    public function countUserMissingBets(User $user = null)
+    {
+        $user = $user ? $user : $this->security->getUser();
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "SELECT COUNT(t1.id) FROM (
+                    SELECT m.*
+                    FROM game m
+                    WHERE m.date > :date
+                ) AS t1
+                LEFT JOIN (
+                    SELECT m.*
+                    FROM game m
+                    LEFT JOIN bet b ON b.game_id = m.ID
+                    WHERE b.user_id = :user
+                ) AS t2
+                ON t1.ID = t2.ID
+                WHERE t2.ID IS NULL";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('date', (new \DateTime("2021-02-06 20:34:00"))->format('Y-m-d H:i:s'));
+        $stmt->bindValue('user', $user->getId());
+        $stmt->execute();
+
+        return $stmt->fetchOne();
+    }
 }

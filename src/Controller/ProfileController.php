178cb3c04e764966ceeec86d65b4745d13e9abad
luk4szm <lukasz@mikowski.pl>
2021-05-03 10:26:33 +0200

    profile controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/profile")
+ * @IsGranted("ROLE_USER")
+ */
 class ProfileController extends AbstractController
 {
 	/**
-	 * @Route("/profile", name="user_profile")
-     * @IsGranted("ROLE_USER")
+	 * @Route("/", name="user_profile")
 	 */
 	public function summary(): Response
 	{
@@ -23,4 +29,24 @@ class ProfileController extends AbstractController
 			'user' => $user
 		]);
 	}
+
+    /**
+     * @Route("/update/{id}", name="user_update", methods={"POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    public function updateUserEmail(Request $request, EntityManagerInterface $em, User $user)
+    {
+        $data = json_decode($request->request->get('data'), true);
+
+        if (isset($data['email'])) {
+            $user->setLastEmail($user->getEmail());
+            $user->setEmail($data['email']);
+        }
+
+        $em->flush();
+
+        return $this->json(['success' => 'ok']);
+    }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -39,21 +36,5 @@ class UserController extends AbstractController
         return $this->render('user/user_info.html.twig', [
             'user' => $user,
         ]);
-    }
-
-    /**
-     * @Route("/user/update_email/{username}", name="user_update_email")
-     * @param Request $request
-     * @param EntityManagerInterface $em
-     * @param User $user
-     * @return JsonResponse
-     */
-    public function updateUserEmail(Request $request, EntityManagerInterface $em, User $user)
-    {
-        $user->setLastEmail($user->getEmail());
-        $user->setEmail($request->request->get('email'));
-        $em->flush();
-
-        return $this->json(['success' => 'ok']);
     }
 }

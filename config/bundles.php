7816d61805e8a4f2f7a45b51d04b9d7f210c022e
luk4szm <lukasz@mikowski.pl>
2021-05-07 23:28:37 +0200

    knp-time-bundle install

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];

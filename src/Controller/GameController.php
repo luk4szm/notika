51d1c5fb5c0bd4ad7086a21658b8c63eb4f96930
luk4szm <lukasz@mikowski.pl>
2021-05-09 01:03:53 +0200

    game main info

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Bet;
 use App\Entity\Game;
 use App\Service\ScheduleService;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,9 +33,52 @@ class GameController extends AbstractController
             $game->getRound()
         );
 
+        $userBet = null;
+        foreach ($game->getBets() as $bet) {
+            if ($this->getUser() === $bet->getUser()) {
+                $userBet = $bet;
+                break;
+            }
+        }
+
         return $this->render('game/game.html.twig', [
             'game'          => $game,
             'roundSchedule' => $roundSchedule,
+            'userBet'       => $userBet,
         ]);
+    }
+
+    /**
+     * @Route("/game/{id}/save-bet", name="game_save_bet")
+     * @IsGranted("ROLE_USER")
+     */
+    public function saveBet(Request $request, Game $game): Response
+    {
+        $data = json_decode($request->request->get('data'), true);
+
+        /** @var Bet $bet */
+        $bet = $this->em
+            ->getRepository(Bet::class)
+            ->findOneBy([
+                            'game' => $game,
+                            'user' => $this->getUser(),
+                        ]);
+        $bet = $bet ? $bet : new Bet();
+        $bet->setGoalsHome($data['goalsHome']);
+        $bet->setGoalsGuest($data['goalsGuest']);
+
+        if (!$bet->getId()) {
+            $bet->setUser($this->getUser());
+            $bet->setGame($game);
+            $bet->setCreatedAt(new \DateTimeImmutable());
+
+            $this->em->persist($bet);
+        } else {
+            $bet->setUpdatedAt(new \DateTimeImmutable());
+        }
+
+        $this->em->flush();
+
+        return $this->json(['success' => 'ok']);
     }
 }
